694cfad700c2a987300c5f8d11c0998ee25fc633
Zach <ztilos713@gmail.com>
2022-06-27 01:41:45 -0500

    added random number function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let firstCard = 7;
-let secondCard = 11;
-let cards = [firstCard, secondCard]
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
@@ -10,12 +10,20 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+function getRandomCard() {
+    let randomCard = Math.floor(Math.random() * 11 + 1);
+    return randomCard;
+}
+
 function startGame() {
     renderGame();
 }
 
 function renderGame() {
-    cardsEl.textContent = `Cards: ${cards[0]}  ${cards[1]}`;
+    cardsEl.textContent = `Cards: `;
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
     sumEl.textContent = `Sum: ${sum}`;
     if (sum <= 20) {
     message = 'Hit?';
@@ -31,7 +39,8 @@ function renderGame() {
 }
 
 function newCard() {
-    let newCard = 3;
+    let newCard = getRandomCard();
     sum += newCard;
+    cards.push(newCard);
     renderGame();
 }
